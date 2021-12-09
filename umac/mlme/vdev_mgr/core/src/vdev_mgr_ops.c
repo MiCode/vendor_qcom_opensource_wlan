fd747f668530e70b830c3507771193fe7c9df82a
Santosh Anbu <quic_sanbu@quicinc.com>
2021-12-09 11:10:39 +0530

    qcacmn: Move MLO mcast functionality under AP mode
    Add change to move mlo mast functionality under AP mode check as it is
    specific to SAP mode only.
    
    Change-Id: Ib5254873331a095f69638137e46697d8de352a2f
    CRs-Fixed: 3092320

--- umac/mlme/vdev_mgr/core/src/vdev_mgr_ops.c
+++ umac/mlme/vdev_mgr/core/src/vdev_mgr_ops.c
@@ -311,10 +311,10 @@ vdev_mgr_start_param_update_mlo(struct vdev_mlme_obj *mlme_obj,
 	    !wlan_vdev_mlme_is_mlo_link_vdev(vdev))
 		param->mlo_flags.mlo_assoc_link = 1;
 
-	vdev_mgr_start_param_update_mlo_mcast(vdev, param);
-
-	if (wlan_vdev_mlme_get_opmode(vdev) == QDF_SAP_MODE)
+	if (wlan_vdev_mlme_get_opmode(vdev) == QDF_SAP_MODE) {
+		vdev_mgr_start_param_update_mlo_mcast(vdev, param);
 		vdev_mgr_start_param_update_mlo_partner(vdev, param);
+	}
 }
 #else
 static void
