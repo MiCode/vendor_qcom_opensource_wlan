0dd1799d92d9ad50a2470fc24a8c597a77d7da6d
Himanshu Batra <quic_hbatra@quicinc.com>
2021-12-23 16:52:14 +0530

    qcacmn: Increase IPA Max bandwidth to support high BW voting
    Increase IPA Max bandwidth to support high BW voting
    
    Change-Id: I80d127601d7c6c6feb214062cee6b35a3553f9de

--- ipa/core/inc/wlan_ipa_priv.h
+++ ipa/core/inc/wlan_ipa_priv.h
@@ -77,7 +77,11 @@
 #define WLAN_IPA_UC_STA_ENABLE_MASK         BIT(6)
 #define WLAN_IPA_REAL_TIME_DEBUGGING        BIT(8)
 
+#ifdef QCA_IPA_LL_TX_FLOW_CONTROL
+#define WLAN_IPA_MAX_BANDWIDTH              4800
+#else
 #define WLAN_IPA_MAX_BANDWIDTH              800
+#endif
 
 #define WLAN_IPA_MAX_PENDING_EVENT_COUNT    20
 
