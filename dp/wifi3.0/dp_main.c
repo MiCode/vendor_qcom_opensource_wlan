84e111db2a5034ad1ffad377fe640e5615eaae96
Harsh Kumar Bijlani <quic_hbijlani@quicinc.com>
2022-01-04 04:07:10 -0800

    Revert "qcacmn: Use atomic variant of qdf_mem_malloc for vdev stats"
    This reverts commit Ia36a41deb5ba882adb0727538c085c33c7e38b7c.
    
    Change-Id: I01b43f4552c4101e4d81ccc4ccb6c6d2405e5cdc
    CRs-Fixed: 3103009

--- dp/wifi3.0/dp_main.c
+++ dp/wifi3.0/dp_main.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021,2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -8478,7 +8478,7 @@ static QDF_STATUS dp_vdev_getstats(struct cdp_vdev *vdev_handle,
 
 	soc = pdev->soc;
 
-	vdev_stats = qdf_mem_malloc_atomic(sizeof(struct cdp_vdev_stats));
+	vdev_stats = qdf_mem_malloc(sizeof(struct cdp_vdev_stats));
 
 	if (!vdev_stats) {
 		dp_cdp_err("%pK: DP alloc failure - unable to get alloc vdev stats",
