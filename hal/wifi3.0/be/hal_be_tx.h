fc1320b1fa34a2c96a18ef6e08e6b33fc39cf183
Neelansh Mittal <quic_neelansh@quicinc.com>
2022-01-17 14:15:45 +0530

    qcacmn: Fix LF Copyright Markings
    Fix LF Copyright Markings
    
    Change-Id: I450e95a08a4cf6105932e98486a185ea1fb2dabf

--- hal/wifi3.0/be/hal_be_tx.h
+++ hal/wifi3.0/be/hal_be_tx.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2022 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2021 The Linux Foundation. All rights reserved.
  * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
