94a233e8e6ee396e18cf3f6fb74a4ec4242d0a3e
Kiran Venkatappa <quic_kiranv@quicinc.com>
2022-02-04 23:36:54 +0530

    qcacmn: Move mlo setup trigger to end of WMI ready handler
    MLO setup trigger is required to trigger setup sequence with FW. Move it
    to end of the function to ensure that it is always called and skipped
    due to goto
    
    Change-Id: I49d096f6e9c4ae0d9ad0de5e1ab32eaaaa3fee2e
    CRs-Fixed: 3124260

--- target_if/init_deinit/src/init_event_handler.c
+++ target_if/init_deinit/src/init_event_handler.c
@@ -793,10 +793,10 @@ static int init_deinit_ready_event_handler(ol_scn_t scn_handle,
 			wlan_psoc_set_hw_macaddr(psoc, myaddr);
 	}
 
-	init_deinit_mlo_update_pdev_ready(psoc, num_radios);
 out:
 	target_if_btcoex_cfg_enable(psoc, tgt_hdl, event);
 	tgt_hdl->info.wmi_ready = TRUE;
+	init_deinit_mlo_update_pdev_ready(psoc, num_radios);
 exit:
 	init_deinit_wakeup_host_wait(psoc, tgt_hdl);
 
