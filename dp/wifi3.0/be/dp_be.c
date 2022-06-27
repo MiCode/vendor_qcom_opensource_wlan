720b38fbc6034c05c1acb3ab8a1ea01fca1c608a
Jia Ding <quic_jiad@quicinc.com>
2022-06-27 11:36:40 +0800

    qcacmn: Use correct wbm_ring_num for SW5 and SW6
    With KIWI_V2, wbm_ring_num for WBM2SW5 and WBM2SW6 have been changed
    to 5 and 6. Hence properly update them in g_tcl_wbm_map_array. At the
    same time, tx_ring_mask_msi and tx_ring_near_full_irq_mask are also
    updated.
    
    With IPA_OFFLOAD enabled soc->tcl_data_ring[0|1|2] is used by HOST
    and the other two rings are allocated to IPA usage.
    
    Change-Id: I4c13d0787e46be667c3a5a0ae624df8c2b2b354e
    CRs-Fixed: 3229375

--- dp/wifi3.0/be/dp_be.c
+++ dp/wifi3.0/be/dp_be.c
@@ -38,8 +38,13 @@ static struct wlan_cfg_tcl_wbm_ring_num_map g_tcl_wbm_map_array[MAX_TCL_DATA_RIN
 	{.tcl_ring_num = 0, .wbm_ring_num = 0, .wbm_rbm_id = HAL_BE_WBM_SW0_BM_ID, .for_ipa = 0},
 	{1, 4, HAL_BE_WBM_SW4_BM_ID, 0},
 	{2, 2, HAL_BE_WBM_SW2_BM_ID, 0},
+#ifdef QCA_WIFI_KIWI_V2
+	{3, 5, HAL_BE_WBM_SW5_BM_ID, 0},
+	{4, 6, HAL_BE_WBM_SW6_BM_ID, 0}
+#else
 	{3, 6, HAL_BE_WBM_SW5_BM_ID, 0},
 	{4, 7, HAL_BE_WBM_SW6_BM_ID, 0}
+#endif
 };
 #else
 #define DP_TX_VDEV_ID_CHECK_ENABLE 1
