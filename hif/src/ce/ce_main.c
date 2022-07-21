81603fe08ca4aab84c7d66a16f69b782c9ce65ec
Rakesh Pillai <quic_pillair@quicinc.com>
2022-07-21 06:10:03 -0700

    qcacmn: Fix format specifier for macro which uses BIT()
    BIT() is unsigned long int, and is being printed using
    an incorrect format specifier. This leads to a compilation
    failure.
    
    Fix the format specifier for macro which uses BIT().
    
    Change-Id: I42a71c3c4a388285809ad17dc62a50bf2df13a5f
    CRs-Fixed: 3249816

--- hif/src/ce/ce_main.c
+++ hif/src/ce/ce_main.c
@@ -2039,7 +2039,7 @@ static struct hif_ce_desc_event *
 {
 	struct ce_desc_hist *ce_hist = &scn->hif_ce_desc_hist;
 
-	hif_debug("get ce debug buffer ce_id %u, only_ce2/ce3=%d, idx=%u",
+	hif_debug("get ce debug buffer ce_id %u, only_ce2/ce3=0x%x, idx=%u",
 		  ce_id, IS_CE_DEBUG_ONLY_FOR_CRIT_CE,
 		  ce_hist->ce_id_hist_map[ce_id]);
 	if (IS_CE_DEBUG_ONLY_FOR_CRIT_CE &&
