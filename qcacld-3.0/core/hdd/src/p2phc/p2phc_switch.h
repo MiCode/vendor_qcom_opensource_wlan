7fd7e602019ab9a337248c38e34d021c394f95c4
贾圣胤 <jiashengyin@xiaomi.com>
2023-06-08 15:44:55 +0800

    Kernel: Xiaomi kernel_wlan changes for Xiaomi 13 Ultra Android T The Patch based on QCOM release TAG:LA.VENDOR.13.2.0.r1-07300-KAILUA.0
    Signed-off-by: 贾圣胤 <jiashengyin@xiaomi.com>

---
+++ qcacld-3.0/core/hdd/src/p2phc/p2phc_switch.h
@@ -0,0 +1,13 @@
+#ifndef _P2PHC_SWITCH_H_
+#define _P2PHC_SWITCH_H_
+
+int hdd_sysfs_p2phc_switch_create(struct kobject *p2phc_kobject);
+void hdd_sysfs_p2phc_switch_destroy(struct kobject *p2phc_kobject);
+
+extern int sysctl_p2phc_enable;
+
+extern int sysctl_p2phc_debug;
+
+extern int sysctl_p2phc_interval;
+
+#endif
